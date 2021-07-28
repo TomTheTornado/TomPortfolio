ba78cd9d46b98e9978c555f6cf4b022c7b70baa3
TomTheTornado <tksp98@gmail.com>
2021-07-27 21:18:33 -0500

    updated profile and projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,15 +9,19 @@ function Projects() {
     <div>
       <Container>
         <h1>Projects</h1>
-        <p>Take a look at some of the projects I've worked on over the years.</p>
+        <p className="d-flex justify-content-center">Take a look at some of the projects I've worked on over the years.</p>
         <Row>
           <Col><img src=""/></Col>
-          <Col>Description of the thing worked on BossFightBattleGround</Col>
+          <Col><a href="">Boss Fight Battlegrounds</a><p> - C# - A 2D multiplayer side scrolling action game created with the Monogame framework.</p></Col>
         </Row>
         <br/>
         <Row>
           <Col><img src=""/></Col>
-          <Col>Description of the thing worked on SpriteMaker</Col>
+          <Col><a href="">Sprite Creator</a><p> - Javascript - A website for creating pixel art, and animations.</p></Col>
+        </Row>
+        <Row>
+          <Col><img src=""/></Col>
+          <Col><a href="https://adventuresconquest.herokuapp.com/">Adventurer's Conquest</a><p> - Javascript - A multiplayer game that I helped contribute to at the Spring 2018 ISU Hackathon. This project won first place with the software section. My role was to design the art and help as backup coding.</p></Col>
         </Row>
       </Container>
     </div>
