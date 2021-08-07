52778c16322867739dcf44e7e6d2540e06b8a8a7
TomTheTornado <tksp98@gmail.com>
2021-08-07 13:22:00 -0500

    redesigned 404 page

--- src/components/404.js
+++ src/components/404.js
@@ -6,9 +6,19 @@ import Button from 'react-bootstrap/Button';
 
 function PageNotFound() {
   return (
-    <div>
-      <h1>404 Error Page Not Found</h1>
-    </div>
+    <Container className="d-flex align-items-center height404">
+      <Col>
+        <Row>
+          <Col><img src='./error404.png' alt="404 Error Image"/></Col>
+        </Row>
+        <br/>
+        <Row>
+          <Col><h3>We searched around far and wide but couldn't find the page you wanted. Please check the URL, or use the navbar above.</h3></Col>
+          
+        </Row>
+      </Col>
+    </Container>
+
   );
 }
 
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,4 @@
-import temp from '../profile.png';
+
 
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col';
